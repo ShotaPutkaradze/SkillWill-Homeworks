45b5615f558285a34d966d88e7601f1a5bef3712
putkaradzeshot@gmail.com <putkaradzeshot@gmail.com>
2023-01-04 11:01:47 +0400

    04 01 2023

--- todo list react/todo/src/App.js
+++ todo list react/todo/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import AddItemForm from "./components/AddItemForm";
+import "./App.css";
+import ToDoList from "./components/ToDoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // state = {
+  //   subject: "1",
+  //   task: "2",
+  // };
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <AddItemForm />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+//subject={this.state.subject} task={this.state.task}
---
+++ todo list react/todo/src/components/ToDoList.js
@@ -0,0 +1,15 @@
+import "./ToDoList.css";
+
+const ToDoList = ([subject, task]) => {
+  return (
+    <div>
+      <header></header>
+      <p></p>
+      <button type="submit">delete</button>
+      <button type="submit">done</button>
+    </div>
+  );
+};
+
+export default ToDoList;
+//value={task}value={subject}
